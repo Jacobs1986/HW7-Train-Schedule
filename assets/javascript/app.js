8c1140dd4f2e4a62e48ae41466de87d67c001815
Sean Jacobson <sean.jacobson86@gmail.com>
2020-01-21 17:34:47 -0700

    Created a variable for the train name and saved it to the database. Had the text area cleared after entry was finished.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,9 +7,26 @@ var firebaseConfig = {
     storageBucket: "fir-demo-9f6b7.appspot.com",
     messagingSenderId: "491402112622",
     appId: "1:491402112622:web:23f52a226ec9cb24dd007f"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  // firebase variable
-  let database = firebase.database();
+// firebase variable
+let database = firebase.database();
+
+// function for the submit button
+$("#submit").on("click", function (event) {
+    // prevent default
+    event.preventDefault();
+    console.log("Clicked")
+    // get the value from each of the items in the form
+    // save them into a variable
+    let name = $("#trainName").val().trim();
+    console.log(name);
+    // save the variables into the firebase database
+    database.ref().push({
+        name: name
+    })
+    // clear each of the text boxes
+    $("#trainName").val("").focus();
+})
